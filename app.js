827502890fb3615b9bbc9e5cca19d0fd179d3656
Matthew <73604168+QWERTYUIOPDADERP@users.noreply.github.com>
2025-02-05 14:12:56 -0800

    javascript for date and pulling from forms and design
    Should be functional. And mobile friendly?

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ let formValues = {
     name: document.getElementById('victimName'),
     user: document.getElementById('userName'),
     date: document.getElementById('userDate'),
+    background: document.getElementById('template'),
     setAllText: function(){
-        certificate.setAllText(this.name.value,this.user.value,this.date.value);
+        if(this.date.value){
+            certificate.setAllText(this.name.value,this.user.value,this.date.value);
+        } else {
+            certificate.setTextNotDate(this.name.value,this.user.value);
+        }
+    },
+    setBackground: function(){
+        console.log(this.background.value);
+        certificate.setBackground(this.background.value);
+    },
+    setAll: function(){
+        this.setAllText();
+        this.setBackground();
     }
 };
 
@@ -11,15 +24,57 @@ let certificate = {
     Name: document.getElementById('victim'),
     user: document.getElementById('user'),
     date: document.getElementById('notedDate'),
+    background: document.getElementById('certificateBackground'),
+
     setText: function(element, text){
         element.innerHTML = text;
     },
     setAllText: function(victimName, userName, date){
-        this.setText(this.Name,victimName);
-        this.setText(this.user,userName);
+        if(victimName){
+            this.setText(this.Name,victimName);
+        }
+        if(userName){
+            this.setText(this.user,userName);
+        }
         this.setText(this.date,date);
     },
+    setTextNotDate: function(victimName, userName){
+        if(victimName){
+            this.setText(this.Name,victimName);
+        }
+        if(userName){
+            this.setText(this.user,userName);
+        }
+    },
+    setDate: function(date){
+        this.setText(this.date,date);
+    },
+    setBackground: function(str){
+        this.background.src = "images/"+str+".png";
+        if(str.includes('Cert')){
+            this.user.style.top = "66%";
+            this.date.style.top = "65%";
+        } else {
+            this.user.style.top = "76%";
+            this.date.style.top = "75%";
+        }
+    },
     print: function(){
-        print()
+        window.print()
     }
 };
+
+function setDate(){
+    let date = new Date();
+    let now = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDay();
+    certificate.setDate(now);
+}
+
+function setCertificate(){
+    formValues.setBackground();
+}
+
+function onLoad(){
+    setDate();
+    setCertificate();
+}
